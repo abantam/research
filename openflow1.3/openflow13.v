904bc1e77668296431aedc8f3ed91a290d12229b
Shimizu Takeshi <abantam.9@gmail.com>
2016-12-23 15:28:06 +0900

    modify topmodule to connect classify_eth_type

--- openflow1.3/openflow13.v
+++ openflow1.3/openflow13.v
@@ -1,6 +1,20 @@
 module openflow13(
-	input [10:0] packet,
+	input clk, rst,
+	input [7:0]   ip_proto,
+	input [15:0]  eth_type, tcp_src, tcp_dst, udp_src, udp_dst,
+	//input [31:0]  in_port, ipv4_src, ipv4_dst,
+	//input [47:0]  eth_src, eth_dst,
+	//input [127:0] ipv6_src, ipv6_dst,
 	output [10:0] bit_vector
+);
+
+wire result;
+
+classify_eth_type cet(
+	.clk(clk),
+	.rst(rst),
+	.eth_type(eth_type),
+	.result(result)
 );
 
 endmodule
